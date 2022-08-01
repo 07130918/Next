8db2cb9318d46d473a6796d764a4160240c270fe
こた <tokyosato1@gmail.com>
2022-08-01 19:11:56 +0900

    useContext

--- hooks-practice/src/App.jsx
+++ hooks-practice/src/App.jsx
@@ -1,8 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import PersonContext from './main';
 import './App.css';
 
 function App() {
     const [count, setCount] = useState(0);
+    const personContext = useContext(PersonContext);
 
     const handleClick = () => {
         setCount(count + 1);
@@ -16,9 +18,13 @@ function App() {
 
     return (
         <div className="App">
-            <h1>UseState, UseEffect</h1>
+            <h1>useState, useEffect</h1>
             <button onClick={handleClick}>+</button>
             <p>{count}</p>
+            <hr />
+            <h1>useContext</h1>
+            <p>{personContext.name}</p>
+            <p>{personContext.age}</p>
         </div>
     );
 }
--- hooks-practice/src/main.jsx
+++ hooks-practice/src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { createContext } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+const person = {
+    name: 'John',
+    age: 20
+}
+
+const PersonContext = createContext(person);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+    <PersonContext.Provider value={person}>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </PersonContext.Provider>
 )
+
+export default PersonContext;
