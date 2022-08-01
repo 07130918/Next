648c4ebef69e834e2312552bbb474b8d91db6883
こた <tokyosato1@gmail.com>
2022-08-02 08:25:41 +0900

    feat: useReducer

--- hooks-practice/src/App.jsx
+++ hooks-practice/src/App.jsx
@@ -1,11 +1,25 @@
-import { useContext, useEffect, useState, useRef } from 'react';
-import PersonContext from './main';
+import { useContext, useEffect, useReducer, useRef, useState } from 'react';
 import './App.css';
+import PersonContext from './main';
+
+const reducer = (state, action) => {
+    // アクション名を決める
+    switch (action.type) {
+        case 'increment':
+            return state + 1;
+        case 'decrement':
+            return state - 1;
+        default:
+            return state;
+    }
+}
 
 function App() {
     const [count, setCount] = useState(0);
     const personContext = useContext(PersonContext);
     const inputText = useRef();
+    // [新たに更新されたstate, 通知の合図]を返す
+    const [state, dispatch] = useReducer(reducer, 0);
 
     const handleClick = () => {
         setCount(count + 1);
@@ -20,7 +34,6 @@ function App() {
         console.log(inputText.current.value);
     }
 
-
     return (
         <div className="App">
             <h1>useState, useEffect</h1>
@@ -32,8 +45,13 @@ function App() {
             <p>{personContext.age}</p>
             <hr />
             <h1>useRef</h1>
-            <input type="text" ref={inputText}/>
+            <input type="text" ref={inputText} />
             <button onClick={handleRef}>Click here!</button>
+            <hr />
+            <h1>useReducer</h1>
+            <p>カウント: {state}</p>
+            <button onClick={() => dispatch({ type: 'increment' })}>+</button>
+            <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
         </div>
     );
 }
