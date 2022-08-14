daa94badbc325b170b906b960eba2fb6921b5073
こた <tokyosato1@gmail.com>
2022-08-15 08:43:59 +0900

    object state

--- complete-tutorial/05_state_and_event/src/020_useState/end/Example.js
+++ complete-tutorial/05_state_and_event/src/020_useState/end/Example.js
@@ -1,24 +1,23 @@
 import { useState } from "react";
 
 const Example = () => {
-  // POINT useStateは[ 値、変更用の関数 ]を返す
-  // let valArry = useState();
-  // POINT 分割代入で取得
-  let [val, setVal] = useState();
+    // POINT useStateは[値、変更用の関数]を返す
+    let [val, setVal] = useState("初期値");
 
-  return (
-    <>
-      <input
-        type="text"
-        /* POINT 入力状態をstateで管理する標準的な書き方 */
-        onChange={(e) => {
-          // const setFn = valArry[1];
-          setVal(e.target.value);
-        }}
-      />
-      = {val}
-    </>
-  );
+    return (
+        <>
+            <input
+                type="text"
+                /* POINT 入力状態をstateで管理する標準的な書き方 */
+                onChange={(e) => {
+                    setVal(e.target.value);
+                }}
+            />
+            <div>
+                {val}
+            </div>
+        </>
+    );
 };
 
 export default Example;
