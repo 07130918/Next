c70f984358ef0475591e58b118696c825a765ccf
こた <tokyosato1@gmail.com>
2022-08-21 14:28:13 +0900

    merge

--- complete-tutorial/19_test/src/040_test_suite/start/components/Counter.test.js
+++ complete-tutorial/19_test/src/040_test_suite/start/components/Counter.test.js
@@ -1,22 +1,6 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import Counter from "./Counter";
 
-<<<<<<< HEAD
-describe('テストのグループ名', () => { });
-
-test('「カウントアップ」押下で「現在のカウント」が+1されるか', () => {
-    render(<Counter originCount={0} />);
-
-    const spanElBeforeUpdate = screen.getByText('現在のカウント:0')
-    expect(spanElBeforeUpdate).toBeInTheDocument();
-
-    const btn = screen.getByRole('button', { name: 'カウントアップ' });
-    fireEvent.click(btn);
-
-    const spanEl = screen.getByText('現在のカウント:1')
-    expect(spanEl).toBeInTheDocument();
-})
-=======
 // describeでテストをまとめて実行が失敗したときにわかりやすくする
 describe('カウンターコンポーネントの動作確認', () => {
     describe('ボタンの制御確認', () => {
@@ -60,4 +44,3 @@ describe('カウンターコンポーネントの動作確認', () => {
         });
     });
 });
->>>>>>> 4a5ee5b6fa19f16d331e97ff60fa8cca0d75c714
