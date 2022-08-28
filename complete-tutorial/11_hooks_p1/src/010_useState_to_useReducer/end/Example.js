eea636a076b0d8d39d49024487a4445e7c84ae95
こた <tokyosato1@gmail.com>
2022-08-28 15:26:59 +0900

    useReducer

--- complete-tutorial/11_hooks_p1/src/010_useState_to_useReducer/end/Example.js
+++ complete-tutorial/11_hooks_p1/src/010_useState_to_useReducer/end/Example.js
@@ -1,49 +1,45 @@
 import { useReducer, useState } from "react";
 /* POINT useReducerの使い方
 const [state, dispatch] = useReducer(reducer, initialArg, init)
-reducer(state, action)
+reducer(state, action(一般的にはオブジェクトが入る))
 */
 // POINT useReducerはuseStateの書き換えに使用
 const Example = () => {
-  const [state, setState] = useState(0);
-  const [rstate, dispatch] = useReducer((prev, { type, step }) => {
-    switch (type) {
-      case "+":
-        return prev + step; 
-      case "-":
-        return prev - step;
-      default:
-        throw new Error('不明なactionです。')
-    }
-    // if (action === "+") {
-    //   return ++prev;
-    // } else if (action === "-") {
-    //   return --prev;
-    // }
-  }, 0);
+    const [state, setState] = useState(0);
+    // useReducerは定義時に値の更新の仕方まで決める
+    const [rstate, dispatch] = useReducer((prevState, { type, step }) => {
+        switch (type) {
+            case "+":
+                return prevState + step;
+            case "-":
+                return prevState - step;
+            default:
+                throw new Error('不明なactionです。')
+        }
+    }, 0);
 
-  const countUp = () => {
-    setState((prev) => ++prev);
-  };
-  const rcountUp = () => {
-    dispatch({ type: "+", step: 2 });
-  };
-  const rcountDown = () => {
-    dispatch({ type: "-", step: 3 });
-  };
-  return (
-    <>
-      <div>
-        <h3>{state}</h3>
-        <button onClick={countUp}>+</button>
-      </div>
-      <div>
-        <h3>{rstate}</h3>
-        <button onClick={rcountUp}>+</button>
-        <button onClick={rcountDown}>-</button>
-      </div>
-    </>
-  );
+    const countUp = () => {
+        setState(state + 1);
+    };
+    const rcountUp = () => {
+        dispatch({ type: "+", step: 2 });
+    };
+    const rcountDown = () => {
+        dispatch({ type: "-", step: 3 });
+    };
+    return (
+        <>
+            <div>
+                <h3>{state}</h3>
+                <button onClick={countUp}>+</button>
+            </div>
+            <div>
+                <h3>{rstate}</h3>
+                <button onClick={rcountUp}>+</button>
+                <button onClick={rcountDown}>-</button>
+            </div>
+        </>
+    );
 };
 
 export default Example;
