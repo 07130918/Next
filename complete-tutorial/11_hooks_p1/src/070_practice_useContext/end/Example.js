73c6ce219fe343042b658887758bfe89694acb74
こた <tokyosato1@gmail.com>
2022-08-30 08:56:17 +0900

    useContext, useReducerの練習

--- complete-tutorial/11_hooks_p1/src/070_practice_useContext/end/Example.js
+++ complete-tutorial/11_hooks_p1/src/070_practice_useContext/end/Example.js
@@ -1,19 +1,17 @@
-// POINT Contextの練習問題
-import Input from "./components/Input"
-import Select from "./components/Select"
-import Result from "./components/Result.js"
+import Input from "./components/Input";
+import Result from "./components/Result.js";
+import Select from "./components/Select";
 import { CalcProvider } from "./context/CalcContext";
 
 const Example = () => {
-  
-  return (
-    <CalcProvider>
-      <Input name="a"/>      
-      <Input name="b" />      
-      <Select />
-      <Result />      
-    </CalcProvider>
-  );
+    return (
+        <CalcProvider>
+            <Input name="a" />
+            <Input name="b" />
+            <Select />
+            <Result />
+        </CalcProvider>
+    );
 };
 
 export default Example;
